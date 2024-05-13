c9fdf855bc1d731962f6f569c9f53fe84aa37b04
Junzhe Wu <junzhewu@umich.edu>
2024-05-13 13:31:40 -0400

    fix printing bugs

--- rkhs_splatting/trainer.py
+++ rkhs_splatting/trainer.py
@@ -160,9 +160,8 @@ class Trainer(object):
                 total_loss = total_loss.item()
                 log_str = f'loss: {total_loss:.3f}'
                 
-                print(log_dict)
                 for k in log_dict.keys():
-                    log_str += " {}: {:.3f}".format(k, log_dict[k])
+                    log_str += " {}: {:.3f}".format(k, float(log_dict[k]))
                 
                 pbar.set_description(log_str)
 
